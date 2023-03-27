759adc9e88d98ed88e0dea4983bd08852ad033ae
Daniel <123590342+bisuketto0@users.noreply.github.com>
2023-03-27 09:57:52 -0300

    Change eraser method to erase

--- script.js
+++ script.js
@@ -78,7 +78,9 @@ function drawClick(e) {
   if (pen) {
     e.target.style.backgroundColor = color
   } else if (eraser) {
-    e.target.style.backgroundColor = 'white'
+    if (e.target.style.backgroundColor) {
+      e.target.style.backgroundColor = null
+    }
   } else if (rainbow) {
     let rainbowColor = getRandomColor()
     e.target.style.backgroundColor = rainbowColor
@@ -91,7 +93,9 @@ function draw(e) {
     if (pen) {
       e.target.style.backgroundColor = color
     } else if (eraser) {
-      e.target.style.backgroundColor = 'white'
+      if (e.target.style.backgroundColor) {
+        e.target.style.backgroundColor = null
+      }
     } else if (rainbow) {
       let rainbowColor = getRandomColor()
       e.target.style.backgroundColor = rainbowColor
